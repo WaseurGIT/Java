4a672dcba71de8ea337e0e36e6648219b5956ae1
WaseurGIT <waseurrahman31@gmail.com>
2024-10-08 11:14:12 +0600

    added something in parent

--- OOP/Mid_Question_Solve/Solve_5a/Child.java
+++ OOP/Mid_Question_Solve/Solve_5a/Child.java
@@ -1,16 +1,20 @@
 public class Child extends Parent {
     public void display() {
+        super.display();
         // num = 30; // Remove this line as num is final
         System.out.println("This is in Child");
     }
 
     public void test() {
-        Child ob = new Child();
 
-        ob.display();
+        this.display();
 
-        System.out.println("variable in Child: " + ob.num);
+        System.out.println("variable in Child: " + this.num);
         System.out.println("variable in Parent: " + super.num);
+
+        // if num is private in parent class
+        // System.out.println("variable in Child: " + this.getNum());
+        // System.out.println("variable in Parent: " + super.getNum());
     }
 
     public static void main(String args[]) {
--- OOP/Mid_Question_Solve/Solve_5a/Parent.java
+++ OOP/Mid_Question_Solve/Solve_5a/Parent.java
@@ -1,7 +1,13 @@
 class Parent {
+    // private final int num = 20;
     public final int num = 20;
-
+    
     public void display() {
         System.out.println("This is in Parent");
     }
+
+    // if num is private
+    // public int getNum(){
+    //     return num;
+    // }
 }
