6259343f1fb92424095af975fe5b6eddd687b5fb
WaseurGIT <waseurrahman31@gmail.com>
2024-11-06 12:50:32 +0600

    create directory, file and write something inside file

---
+++ Practice/List_Map_File/CreateDirectory_1.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+
+public class CreateDirectory_1 {
+    public static void main(String[] args) {
+
+        // to create a directory
+        File dir = new File("Java_Directory_1");
+        dir.mkdir();  // directory created 
+
+        String dirPath = dir.getAbsolutePath(); // to see the directory path
+        System.out.println(dir.getName()); //  to see the directory name
+        
+
+        //to create a file inside a directory
+        File file1 = new File(dirPath+"\\hello.txt" );
+
+        try {
+            file1.createNewFile();
+            System.out.println("File created successfully");
+        } 
+        catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+
+
+        // ** uncomment this to delete the file **
+        // file1.delete();
+        
+        // ** uncomment this to delete the directory **
+        // if(dir.delete()){
+        //     System.out.println("Directory deleted");
+        // }
+
+        // ** uncomment this to check is the file exists or not **
+        // if (file1.exists()) {
+        //     System.out.println("File exists");
+        // }
+
+        // to write something inside a file
+
+        try {
+            Formatter formatter = new Formatter(dirPath+"\\hello.txt");
+            formatter.format("%s\r\n", "Hello World!!!");
+            formatter.format("%s\r\n", "Learning java ");
+            formatter.format("%s\t\n", "Watching video in Youtube of Anisul Islam ");
+            formatter.format("%s","Bangladesh University of Business and technology");
+            formatter.close();
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
