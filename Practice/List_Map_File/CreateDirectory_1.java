7d88fa88ec30b943a91b7b0721659e1179d0a2b9
WaseurGIT <waseurrahman31@gmail.com>
2024-11-06 23:15:51 +0600

    read data from file

--- Practice/List_Map_File/CreateDirectory_1.java
+++ Practice/List_Map_File/CreateDirectory_1.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Formatter;
+import java.util.Scanner;
 
 public class CreateDirectory_1 {
     public static void main(String[] args) {
@@ -50,6 +51,20 @@ public class CreateDirectory_1 {
         } 
         catch (FileNotFoundException e) {
             System.out.println("Error: "+e.getMessage());
+        }
+
+        // to see the context of a file
+        System.out.println("\nContent of a file:");
+        try {
+            Scanner sc = new Scanner(file1);
+            while (sc.hasNext()){
+                String s1 = sc.next();
+                System.out.print(s1+" ");
+                sc.close();
+            }
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("Error: "+e);
         }
     }
 }
