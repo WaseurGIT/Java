90da0ddc4e7666e88a607eda8cbe2c9d5b819a8c
WaseurGIT <waseurrahman31@gmail.com>
2024-11-11 22:12:22 +0600

    okay bye!

--- Practice/Fi_nal.java
+++
@@ -1,7 +0,0 @@
-public class Fi_nal {
-    final double PI = 3.1416;
-    public static void main(String[] args) {
-        Fi_nal f1 = new Fi_nal();
-        System.out.println("Pi : "+f1.PI);
-    }
-}
