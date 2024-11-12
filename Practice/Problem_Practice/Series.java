fd031acdeebfa12b74da9b5c0a4c8d6447e09a7f
WaseurGIT <waseurrahman31@gmail.com>
2024-11-12 09:24:18 +0600

    fix a code

--- Practice/Problem_Practice/Series.java
+++ Practice/Problem_Practice/Series.java
@@ -1,20 +1,22 @@
-package Problem_Practice;
-
 import java.util.Scanner;
 
 public class Series {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
 
-        int dif1 = num2 - num1;
-        int dif2 = num3 - num2;
+
+        int dif1 = a[1] - a[0];
+        int dif2 = a[2] - a[1];
 
         if(dif1 == dif2){
-            int newNumber = num3 + dif1;
+            int newNumber = a[n-1] + dif1;
             System.out.println(newNumber);
         }
         sc.close();
