9041a10d114e12628863772ae1352417aab4005e
WaseurGIT <waseurrahman31@gmail.com>
2025-04-15 17:09:15 +0600

    nothing

--- Practice/CodeForces/Palindrome.java
+++ Practice/CodeForces/Palindrome.java
@@ -4,20 +4,25 @@ public class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String str1 = sc.nextLine();
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0;i<t;i++){
+
+            String str1 = sc.nextLine();
+            str1 = str1.toLowerCase();
+
+            String str2 = new StringBuilder(str1).reverse().toString();
+
+            
+            if (str1.equals(str2)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
         sc.close();
-        String str2 = "";
-
-        str1 = str1.toLowerCase();
-
-        for (int i = str1.length() - 1; i >= 0; i--) {
-            str2 += str1.charAt(i);
-        }
-
-        if (str1.equals(str2)) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
+        
     }
 }
